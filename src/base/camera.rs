35d27c9f95004420fff3d8445d6926274e781801
Keitaro Oguri <ogukei256@gmail.com>
2020-06-27 18:30:43 +0900

    Adjust initial camera position

--- src/base/camera.rs
+++ src/base/camera.rs
@@ -10,17 +10,22 @@ pub struct OrbitalCamera {
     inv_proj: glm::Mat4x4,
     rotation_x: f32,
     rotation_y: f32,
+    distance: f32,
 }
 
 impl OrbitalCamera {
     pub fn new() -> Self {
         let perspective = glm::perspective_fov(glm::radians(&glm::vec1(60.0)).x, 1.0, 1.0, 0.1, 100.0);
         let inv_proj = glm::inverse(&perspective);
+        let rotation_x: f32 = -0.24;
+        let rotation_y: f32 = 0.36;
+        let distance: f32 = 3.0;
         Self {
-            view: orbital(3.0, 0.0, 0.0),
+            view: orbital(distance, rotation_x, rotation_y),
             inv_proj: inv_proj,
-            rotation_x: 0.0,
-            rotation_y: 0.0,
+            rotation_x,
+            rotation_y,
+            distance,
         }
     }
 
@@ -44,7 +49,7 @@ impl OrbitalCamera {
     fn rotate(&mut self, x: f32, y: f32) {
         self.rotation_x += (x / 400.0) * glm::pi::<f32>();
         self.rotation_y += (y / 400.0) * glm::pi::<f32>();
-        self.view = orbital(3.0, self.rotation_x, self.rotation_y);
+        self.view = orbital(self.distance, self.rotation_x, self.rotation_y);
     }
 }
 
